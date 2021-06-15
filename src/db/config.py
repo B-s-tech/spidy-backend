691866076c900b8209710a08b3fc58bf93495d34
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-06-16 01:47:02 +0900

    setup postgres

--- src/db/config.py
+++ src/db/config.py
@@ -1,11 +1,11 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{password}@{host}:{port}/{db_name}'.format(**{
-  'user': 'admin',
-  'password': 'admin',
-  'host': 'spidy-backend_db_1',
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}'.format(**{
+  'user': 'postgres',
+  'password': 'postgres',
+  'host': 'spidy-db',
   'port': '5432',
-  'db_name': 'spidy.db'
+  'db_name': 'db'
 })
 SECRET_KEY = os.urandom(24)
 
